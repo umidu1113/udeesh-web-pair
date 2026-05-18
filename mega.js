e9a18c856e73c473a5ca04281a3609bcaaf220d0
umidu <150492790+umidu1113@users.noreply.github.com>
2026-05-18 11:44:19 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "u83386653@gmail.com",
+  password: "Slrc@12345",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
